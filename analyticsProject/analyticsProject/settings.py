1385fde9c0552990244936da77aaa31fe57d69a3
Vivek Kumar Rahul <vivekrahul91@gmail.com>
2023-10-16 01:38:17 +0530

    added html and css files in template

--- analyticsProject/analyticsProject/settings.py
+++ analyticsProject/analyticsProject/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_FILE_DIR = [
+    BASE_DIR, "static"
+]
