3868625949f90f3f3b951f0dceed92dc29088148
Vivek Kumar Rahul <vivekrahul91@gmail.com>
2023-10-16 02:06:41 +0530

    added a dashboard and lots of debugs to make it working

--- analyticsProject/analyticsProject/urls.py
+++ analyticsProject/analyticsProject/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-urlpatterns = [
 
+urlpatterns = [
+    path('', include('dashboard.urls')),
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
 
