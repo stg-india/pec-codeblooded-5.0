dfa9e2fc6c382a7861f70585237bd1562173fb9d
Asmit Saha <asmitsaha06@gmail.com>
2023-10-15 12:47:42 +0530

    authentication

---
+++ Project/client/src/pages/register/Register.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./Register.scss"
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
+import upload from "../../utils/upload";
+
+const Register = () => {
+  const [file, setFile] = useState(null);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUser((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleSeller = (e) => {
+    setUser((prev) => {
+      return { ...prev, isSeller: e.target.checked };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const url = await upload(file);
+    try {
+      await newRequest.post("/auth/register", {
+        ...user,
+        img: url,
+      });
+      navigate("/")
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="register">
+      <form onSubmit={handleSubmit}>
+        <div className="left">
+          <h1>Create a new account</h1>
+          <label htmlFor="">Username</label>
+          <input
+            name="username"
+            type="text"
+            placeholder="your name"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Password</label>
+          <input name="password" type="password" onChange={handleChange} />
+          <button type="submit">Register</button>
+        </div>
+
+      </form>
+    </div>
+  )
+}
+
+export default Register
+
